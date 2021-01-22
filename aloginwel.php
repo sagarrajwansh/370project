15691ecc1ef4e2ba0a4f96452a10ef9e0eb372b5
sagarrajwansh <66787959+sagarrajwansh@users.noreply.github.com>
2021-01-22 23:09:44 +0530

    Update aloginwel.php

--- aloginwel.php
+++ aloginwel.php
@@ -1,5 +1,5 @@
 <?php 
-require_once ('process/dbh.php');
+require_once ('dbh.php');
 $sql = "SELECT id, firstName, lastName,  points FROM employee, rank WHERE rank.eid = employee.id order by rank.points desc";
 $result = mysqli_query($conn, $sql);
 ?>
